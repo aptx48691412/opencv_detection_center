856f2e0192ce5f51da5a324cb818e68d58bf1d9e
aptx48691412 <60962088+aptx48691412@users.noreply.github.com>
2021-08-05 03:38:20 +0900

    Update otsu_center.py

--- otsu_center.py
+++ otsu_center.py
@@ -1,5 +1,6 @@
 import numpy as np
 import cv2
+from matplotlib import pyplot as plt
 
 cap=cv2.VideoCapture(0)
 
@@ -22,6 +23,8 @@ while True:
         diff=cv2.absdiff(gray,cv2.convertScaleAbs(first_frame))
         ret,threshold=cv2.threshold(diff,0,255,cv2.THRESH_OTSU)
         cv2.imshow('Frame',threshold)
+        
+
     
     else:
         avg+=1
@@ -35,6 +38,38 @@ while True:
 
     if key==ord('u'):
         print('Amazing')
+
+    if key==ord('h'):
+        # global thresholding
+        # ret1,th1 = cv2.threshold(img,127,255,cv2.THRESH_BINARY)
+
+        # Otsu's thresholding
+        ret2,th2 = cv2.threshold(diff,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+
+        # Otsu's thresholding after Gaussian filtering
+        blur = cv2.GaussianBlur(diff,(5,5),0)
+        ret3,th3 = cv2.threshold(blur,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+
+        # plot all the images and their histograms
+        images = [gray, 0, th2,
+                  blur, 0, th3,
+                  th2, 0, th2,
+                  th3, 0, th3]
+        titles = ['Original Noisy Image','Histogram',"Otsu's Thresholding",
+                  'Gaussian filtered Image','Histogram',"Otsu's Thresholding",
+                  "Otsu's Thresholding",'Histogram',"Otsu's Thresholding",
+                  "Otsu's Thresholding",'Histogram',"Otsu's Thresholding"]
+
+        for i in range(4):
+            plt.subplot(4,3,i*3+1),plt.imshow(images[i*3],'gray')
+            plt.title(titles[i*3]), plt.xticks([]), plt.yticks([])
+            plt.subplot(4,3,i*3+2),plt.hist(images[i*3].ravel(),256)
+            plt.title(titles[i*3+1]), plt.xticks([]), plt.yticks([])
+            plt.subplot(4,3,i*3+3),plt.imshow(images[i*3+2],'gray')
+            plt.title(titles[i*3+2]), plt.xticks([]), plt.yticks([])
+            plt.show()
+        
+
 
 
 
